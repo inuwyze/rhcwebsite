f5a8f9e63234ab6f642144b9f953d61a46afdb4e
qwtfy <paullhungdim7@gmail.com>
2023-02-24 21:15:38 +0530

    initializing project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        // ...
+        '@pinia/nuxt',
+        '@nuxtjs/device',
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/google-fonts',
+        '@nuxtjs/color-mode',
+        '@nuxt/image',
+      ],
+})
